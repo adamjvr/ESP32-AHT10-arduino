a2c2d9ab06bcf686c86459a3445b90f245b40397
adamjvr <fenderstratman@gmail.com>
2023-04-26 19:21:01 -0400

    code updated to explicitly use I2C0 and define the pins, usually wire assumes port 0 as the default but its better to define it in code

--- esp32-aht10/esp32-aht10.ino
+++ esp32-aht10/esp32-aht10.ino
@@ -6,10 +6,13 @@
 #define CMD_SOFTRESET 0xBA
 #define STATUS_BUSY_MASK 0x80
 
+// Define I2C0 pins
+#define SDA_PIN 21
+#define SCL_PIN 22
+
 // Global variables for sensor data
 float temperature = 0.0;
 float humidity = 0.0;
-float dew_point = 0.0;
 
 // Function to read AHT10 sensor
 void readAHT10() {
@@ -40,19 +43,17 @@ void readAHT10() {
   uint8_t cksum = Wire.read();
   uint32_t raw_data = ((msb << 16) | (lsb << 8) | cksum) >> 4;
 
-  // Convert raw data to temperature, humidity, and dew point
+  // Convert raw data to temperature and humidity
   temperature = (float)(raw_data * 200.0 / 1048576.0) - 50.0;
   humidity = (float)(raw_data * 100.0 / 1048576.0);
-  float dew_point_temp = log(humidity / 100.0) + (17.27 * temperature) / (237.7 + temperature);
-  dew_point = (237.7 * dew_point_temp) / (17.27 - dew_point_temp);
 }
 
 void setup() {
   // Initialize serial communication
   Serial.begin(9600);
-  
-  // Initialize I2C communication
-  Wire.begin();
+
+  // Initialize I2C communication on I2C0 with custom SDA and SCL pins
+  Wire.begin(SDA_PIN, SCL_PIN);
   
   // Soft reset AHT10
   Wire.beginTransmission(AHT10_ADDR);
@@ -65,14 +66,12 @@ void loop() {
   // Read data from AHT10
   readAHT10();
 
-  // Print data to serial monitor
+  // Print temperature and humidity values to serial monitor
   Serial.print("Temperature: ");
-  Serial.print(temperature);
+  Serial.print(temperature, 2); // round to 2 decimal places
   Serial.print(" C, Humidity: ");
-  Serial.print(humidity);
-  Serial.print(" %, Dew Point: ");
-  Serial.print(dew_point);
-  Serial.println(" C");
+  Serial.print(humidity, 2); // round to 2 decimal places
+  Serial.println(" %");
 
   // Wait 1 second before taking another measurement
   delay(1000);
